a00accfab199280cd6af1bdcdc1182d7a706c0dc
DanDeLuna <danieldeluna4@gmail.com>
2023-03-30 18:30:00 -0600

    In JS sheet added more code to pull weather data

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -41,3 +41,45 @@ function convertToC(fahrenheit) {
     var celcius = Math.round(cTempVal * 10) / 10;
     return celcius;
   }
+  //Function to get weather details 
+function getWeather(q) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + q + "&units=imperial&appid=" + APIKey;
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+        error: (err => {  
+            alert("Your city was not found. Check your spelling or enter a city code")
+            return;
+          })
+    }).then(function (response) {
+        console.log(response)
+        $(".cityList").empty()
+        $("#days").empty()
+        var celcius = convertToC(response.main.temp);
+        var cityMain1 = $("<div col-12>").append($("<p><h2>" + response.name + ' (' + currentDate + ')' + "</h2><p>"));
+        var image = $('<img class="imgsize">').attr('src', 'http://openweathermap.org/img/w/' + response.weather[0].icon + '.png');        
+        var degreeMain = $('<p>').text('Temperature : ' + response.main.temp + ' °F (' + celcius + '°C)');
+        var humidityMain = $('<p>').text('Humidity : ' + response.main.humidity + '%');
+        var windMain = $('<p>').text('Wind Speed : ' + response.wind.speed + 'MPH');       
+        var uvIndexcoord = '&lat=' + response.coord.lat + '&lon=' + response.coord.lon;
+        var cityId = response.id;
+
+        displayUVindex(uvIndexcoord);
+        displayForecast(cityId);
+
+        cityMain1.append(image).append(degreeMain).append(humidityMain).append(windMain);
+        $('#cityList').empty();
+        $('#cityList').append(cityMain1);
+    });
+}
+function displayUVindex(uv) {
+    $.ajax({
+        url: "https://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + uv,
+        method: "GET"
+    }).then(function (response) {
+        var UVIndex = $("<p><span>");
+        UVIndex.attr("class", "badge badge-danger");
+        UVIndex.text(response.value);
+        $("#cityList").append('UV-Index : ').append(UVIndex);       
+    });
+}
